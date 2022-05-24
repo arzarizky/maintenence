21b913d127274be2cc1c2df3204416be1f48913a
Arza Rizky <panggilsajarizky@gmail.com>
2022-05-24 17:22:33 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no" name="viewport">
+  <title>Print Digital</title>
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="perbaikan.css">
+</head>
+
+<body>
+
+    <div class="aquarium">
+        <div class="aquarium__table"></div>
+        <div class="aquarium__aquarium">
+          <div class="aquarium__water"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+          <div class="aquarium__bubble"></div>
+        </div>
+        <div class="aquarium__drops">
+          <div class="aquarium__drop"></div>
+          <div class="aquarium__water-column"></div>
+          <div class="aquarium__splash"></div>
+          <div class="aquarium__splash"></div>
+        </div>
+        <div class="aquarium__sponge-box">
+          <div class="aquarium__body">
+            <div class="aquarium__body-stripe"></div>
+            <div class="aquarium__body-hole"></div>
+          </div>
+          <div class="aquarium__face">
+            <div class="aquarium__mouth"></div>
+            <div class="aquarium__smile"></div>
+          </div>
+        </div>
+      </div>
+
+    <div>
+        <H1 style="text-align: center; font-family: 'Poppins', sans-serif;"> Web Site Sedang Dalam Perbaikan</H1>
+    </div>
+ 
+</body>
